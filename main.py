9091564e1d5679e8200cc7da4b07378e2d7cee9e
Oliver Castellanos <olivercastellanos@Olivers-MacBook-Air.local>
2026-03-18 12:00:33 -0700

    fix paddle collision logic

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ class Paddle:
     def move(self, direction):
         movement = direction * self.speed
 
-        if self.y - movement >= 0 and self.y - movement <= (HEIGHT - PADDLE_SIZE[1]):
-            self.y -= movement
+        if self.y + movement >= 0 and self.y + movement <= (HEIGHT - PADDLE_SIZE[1]):
+            self.y += movement
     
     def change_speed(self, change):
         self.speed += change
@@ -37,10 +37,10 @@ class Paddle:
 
 class Ball:
 
-    def __init__(self, x, y, direction=[1,1]):
+    def __init__(self, x, y, direction=None):
         self.x = x
         self.y = y
-        self.direction = direction
+        self.direction = direction or [1, 1]
 
     def draw(self, win):
         pygame.draw.circle(win, 'white', (self.x, self.y), BALL_SIZE)
@@ -60,25 +60,59 @@ def draw(win, paddle1, paddle2, ball):
     paddle2.draw(win)
     ball.draw(win)
 
-def check_vertical_collision(paddle, ball):
-    x_ = (paddle.x, paddle.x + PADDLE_SIZE[0])
+def collide(paddle, ball):
+    paddle_rect = pygame.Rect(paddle.x, paddle.y, *PADDLE_SIZE)
+    ball_rect = pygame.Rect(ball.x-BALL_SIZE, ball.y-BALL_SIZE, BALL_SIZE*2, BALL_SIZE*2)
+    return paddle_rect.colliderect(ball_rect)
 
-    top_y = paddle.y - BALL_SIZE
-    bottom_y = paddle.y + PADDLE_SIZE[1] + BALL_SIZE
-    
-    if ball.y in (top_y, bottom_y) and (ball.x >= x_[0] and ball.x <= x_[1]):
-        return True
+def is_left_of_diagonal(paddle_x, paddle_y, change_x, change_y, ball):
+    cross = (change_x) * (ball.y - paddle_y) - (change_y) * (ball.x - paddle_x)
+    return cross <= 0
 
-def check_side_collision(paddle, ball):
-    y_ = (paddle.y, paddle.y + PADDLE_SIZE[1])
+def is_right_of_diagonal(paddle_x, paddle_y, change_x, change_y, ball):
+    cross = (change_x) * (ball.y - paddle_y) - (change_y) * (ball.x - paddle_x)
+    return cross >= 0
 
-    left_x = paddle.x - BALL_SIZE
-    right_x = paddle.x + PADDLE_SIZE[0] + BALL_SIZE
-    
-    if ball.x in (left_x, right_x) and (ball.y >= y_[0] and ball.y <= y_[1]):
-        return True
+def vertical_collision(paddle, ball):
+    if ball.y < paddle.y:
+        if (is_right_of_diagonal(paddle.x, paddle.y, -BALL_SIZE, -BALL_SIZE, ball) and 
+            is_left_of_diagonal(paddle.x + PADDLE_SIZE[0], paddle.y, BALL_SIZE, -BALL_SIZE, ball)):
+            return True
+    elif ball.y > (paddle.y + PADDLE_SIZE[1]):
+        if (is_left_of_diagonal(paddle.x, paddle.y + PADDLE_SIZE[1], -BALL_SIZE, BALL_SIZE, ball) and 
+            is_right_of_diagonal(paddle.x + PADDLE_SIZE[0], paddle.y + PADDLE_SIZE[1], BALL_SIZE, BALL_SIZE, ball)):
+            return True
+    return False
+
+def horizontal_collision(paddle, ball):
+    if ball.x < paddle.x:
+        if (is_left_of_diagonal(paddle.x, paddle.y, -BALL_SIZE, -BALL_SIZE, ball) and 
+            is_right_of_diagonal(paddle.x, paddle.y + PADDLE_SIZE[1], -BALL_SIZE, BALL_SIZE, ball)):
+            return True
+    elif ball.x > (paddle.x + PADDLE_SIZE[0]):
+        if (is_right_of_diagonal(paddle.x + PADDLE_SIZE[0], paddle.y, BALL_SIZE, -BALL_SIZE, ball) and 
+            is_left_of_diagonal(paddle.x + PADDLE_SIZE[0], paddle.y + PADDLE_SIZE[1], BALL_SIZE, BALL_SIZE, ball)):
+            return True
+    return False
+
+def push_ball_vert(paddle, ball):
+    midpoint = paddle.y + (PADDLE_SIZE[1] / 2)
+
+    if ball.y < midpoint:
+        ball.y = paddle.y - BALL_SIZE - 1
+    elif ball.y > midpoint:
+        ball.y = paddle.y + PADDLE_SIZE[1] + BALL_SIZE + 1
+
+def push_ball_horiz(paddle, ball):
+    midpoint = paddle.x + (PADDLE_SIZE[0] / 2)
+
+    if ball.x < midpoint:
+        ball.x = paddle.x - BALL_SIZE - 1
+    elif ball.x > midpoint:
+        ball.x = paddle.x + PADDLE_SIZE[0] + BALL_SIZE + 1
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     Paddle1 = Paddle(100, 100)
     Paddle2 = Paddle(700, 100)
@@ -89,6 +123,7 @@ def main():
 
     while run:
 
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -107,21 +142,34 @@ def main():
                 
                 if event.key == pygame.K_DOWN:
                     move_down = False
+        
+        if collide(Paddle1, Ball_):
+            if vertical_collision(Paddle1, Ball_):    
+                Ball_.bounce(1)
+                move_up = False
+                move_down = False
+                push_ball_vert(Paddle1, Ball_)
+            if horizontal_collision(Paddle1, Ball_):
+                Ball_.bounce(0)
+                push_ball_horiz(Paddle1, Ball_)
 
         if move_up:
-            Paddle1.move(10)
-        if move_down:
             Paddle1.move(-10)
+        if move_down:
+            Paddle1.move(10)
         
-        if Ball_.x == BALL_SIZE or Ball_.x == WIDTH - BALL_SIZE:
+        if Ball_.x <= BALL_SIZE:
+            Ball_.x = BALL_SIZE
             Ball_.bounce(0)
-        if Ball_.y == BALL_SIZE or Ball_.y == HEIGHT - BALL_SIZE:
-            Ball_.bounce(1)
-        
-        if check_vertical_collision(Paddle1, Ball_):
-            Ball_.bounce(1)
-        if check_side_collision(Paddle1, Ball_):
+        if Ball_.x >= WIDTH - BALL_SIZE:
+            Ball_.x = WIDTH - BALL_SIZE
             Ball_.bounce(0)
+        if Ball_.y <= BALL_SIZE:
+            Ball_.y = BALL_SIZE
+            Ball_.bounce(1)
+        if Ball_.y >= HEIGHT - BALL_SIZE:
+            Ball_.y = HEIGHT - BALL_SIZE
+            Ball_.bounce(1)
 
         Ball_.move()
         draw(WIN, Paddle1, Paddle2, Ball_)
